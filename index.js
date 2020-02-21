ecc0c5135cd23a4418d6f6d3a24374c463d9ccab
Alvaro <alvaro.camilloneto@gmail.com>
2020-02-21 00:56:57 -0300

    COmportamento do from

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-import { of, range } from 'rxjs';
+import { from } from 'rxjs';
 
 const observer = {
   next: (val) => console.log('next', val),
   error: (err) => console.log('error', err),
   complete: () => console.log('Complete')
 };
-const source$ = of(1, 2, 3, 4, 5);
-const range$ = range(1, 5);
 
-source$.subscribe(observer);
-range$.subscribe(observer);
+const array$ = from([1, 2, 3, 4, 5]);
+const promise$ = from(fetch('https://api.github.com/users/octocat'));
+
+array$.subscribe(observer);
+promise$.subscribe(observer);
