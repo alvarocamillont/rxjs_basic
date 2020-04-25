005d41b5452168420debd7c7d8621766b3024045
Alvaro Camillo Neto <alvaro.camillo@totvs.com.br>
2020-04-24 23:50:52 -0300

    Criação do exercício

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
-import { interval, timer } from 'rxjs';
+import { interval } from 'rxjs';
+import { scan, mapTo, filter } from 'rxjs/operators';
 
-const observer = {
-  next: (val) => console.log('next', val),
-  error: (err) => console.log('error', err),
-  complete: () => console.log('Complete'),
-};
+const countdown = document.getElementById('countdown');
+const message = document.getElementById('message');
 
-const interval$ = interval(1000);
-const timer$ = timer(0, 1000);
+// streams
+const counter$ = interval(1000);
 
-timer$.subscribe(observer);
-interval$.subscribe(observer);
+counter$
+  .pipe(
+    mapTo(-1),
+    scan((accumulator, current) => {
+      return accumulator + current;
+    }, 10),
+    filter((value) => value >= 0)
+  )
+  .subscribe((value: any) => {
+    countdown.innerHTML = value;
+    if (!value) {
+      message.innerHTML = 'Liftoff!';
+    }
+  });
