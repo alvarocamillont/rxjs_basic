44d190bfe60aaa03677df6d9c595416467ef9d9d
Alvaro <alvaro.camilloneto@gmail.com>
2020-05-01 23:04:19 -0300

    Criação do Exemplo

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,60 @@
-import { interval, timer } from 'rxjs';
+import { combineLatest, fromEvent, of } from 'rxjs';
+import { delay, filter, map, mergeMap, share, tap } from 'rxjs/operators';
 
-const observer = {
-  next: (val) => console.log('next', val),
-  error: (err) => console.log('error', err),
-  complete: () => console.log('Complete'),
+import { calculateMortgage } from './helpers';
+
+// elems
+const loanAmount = document.getElementById('loanAmount');
+const interest = document.getElementById('interest');
+const loanLength = document.querySelectorAll('.loanLength');
+const expected = document.getElementById('expected');
+
+// helpers
+const createInputValueStream = (elem) => {
+  return fromEvent(elem, 'input').pipe(
+    map((event: any) => parseFloat(event.target.value))
+  );
 };
 
-const interval$ = interval(1000);
-const timer$ = timer(0, 1000);
+// simulating a save request
+const saveResponse = (mortageAmount) => {
+  return of(mortageAmount).pipe(delay(1000));
+};
 
-timer$.subscribe(observer);
-interval$.subscribe(observer);
+// streams
+const interest$ = createInputValueStream(interest);
+const loanLength$ = createInputValueStream(loanLength);
+const loanAmount$ = createInputValueStream(loanAmount);
+
+/*
+ * Combine streams of the three values needed to complete
+ * our mortgage calculation. Once all three are filled out
+ * any subsequent updates will trigger a new calculation.
+ */
+const calculation$ = combineLatest(interest$, loanAmount$, loanLength$).pipe(
+  map(([interest, loanAmount, loanLength]) => {
+    return calculateMortgage(interest, loanAmount, loanLength);
+  }),
+  // proving the stream is shared
+  tap(console.log),
+  /*
+   *  If a field is empty, we'll just ignore the update for now
+   *  by filtering out invalid values.
+   */
+  filter((mortageAmount) => !isNaN(mortageAmount)),
+  /*
+   *  Demonstrate sharing a stream so saves won't impact
+   *  display updates. Behind the scenes this uses a Subject,
+   *  which we we learn about in the first lessons of the
+   *  Masterclass course.
+   */
+  share()
+);
+
+calculation$.subscribe((mortageAmount) => {
+  expected.innerHTML = mortageAmount;
+});
+
+calculation$
+  .pipe(mergeMap((mortageAmount) => saveResponse(mortageAmount)))
+  .subscribe();
