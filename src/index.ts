5163f78ad9028534bbcdb0d86b975e631dc8027e
Alvaro <alvaro.camilloneto@gmail.com>
2020-05-01 22:59:48 -0300

    Inclusao de exemplo

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,28 @@
-import { interval, timer } from 'rxjs';
+import { fromEvent, timer } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { exhaustMap, finalize, pluck, switchMapTo, takeUntil, tap } from 'rxjs/operators';
 
-const observer = {
-  next: (val) => console.log('next', val),
-  error: (err) => console.log('error', err),
-  complete: () => console.log('Complete'),
-};
+// Elementos
+const startButton = document.getElementById('start');
+const stopButton = document.getElementById('stop');
+const pollingStatus = document.getElementById('polling-status');
+const dogImage: any = document.getElementById('dog');
 
-const interval$ = interval(1000);
-const timer$ = timer(0, 1000);
+// streams
+const startClick$ = fromEvent(startButton, 'click');
+const stopClick$ = fromEvent(stopButton, 'click');
 
-timer$.subscribe(observer);
-interval$.subscribe(observer);
+startClick$
+  .pipe(
+    exhaustMap(() =>
+      timer(0, 5000).pipe(
+        tap(() => (pollingStatus.innerHTML = 'Ativo')),
+        switchMapTo(
+          ajax.getJSON('https://random.dog/woof.json').pipe(pluck('url'))
+        ),
+        takeUntil(stopClick$),
+        finalize(() => (pollingStatus.innerHTML = 'Parado'))
+      )
+    )
+  )
+  .subscribe((url) => (dogImage.src = url));
